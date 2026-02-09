556f613e775b89c314fcff778df2fe955a3efa8c
jrporto2 <86452124+jrporto2@users.noreply.github.com>
2026-02-08 22:54:00 -0500

    Update permissions and ownership for datadrive
    Change ownership and permissions for datadrive directory.

--- run.sh
+++ run.sh
@@ -49,6 +49,9 @@ fi
 find $DESTINATION -type f -exec chmod 644 {} \;
 find $DESTINATION -type d -exec chmod 755 {} \;
 
+sudo chown -R 999:999 datadrive/postgres/db
+sudo chmod 700 datadrive/
+
 # Run Odoo
 docker compose -f $DESTINATION/docker-compose.yml up -d
 
